89a7083f2b12d87c2499e816514a53b074d170e2
Mai-Lapyst <hello@lapyst.dev>
2024-11-07 01:47:33 +0100

    Add overload to SerdeExceptions constructor

--- src/serde/error.d
+++ src/serde/error.d
@@ -29,6 +29,9 @@ class SerdeException : Exception {
     this(string msg, string file = __FILE__, size_t line = __LINE__, Throwable nextInChain = null) pure nothrow @nogc @safe {
         super(msg, file, line, nextInChain);
     }
+    this(string msg, Throwable nextInChain, string file = __FILE__, size_t line = __LINE__) pure nothrow @nogc @safe {
+        super(msg, file, line, nextInChain);
+    }
 }
 
 class MissingFieldException : SerdeException {
